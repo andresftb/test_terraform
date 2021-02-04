dd668a50bcf88e0e9396d5c4e39678cb7ebfda6b
Andres Trujillo <andresftbz88@gmail.com>
2021-02-03 19:13:10 -0500

    init

---
+++ ejemplo.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0502e817a62226e03"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
+
+output "ip" {
+  value = aws_eip.ip.public_ip
+}
